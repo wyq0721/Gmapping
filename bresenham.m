05cfb453de3dbc9cd19632adf72b8f77cfece4ec
wyq0721 <7151088+wyq0721@users.noreply.github.com>
2019-03-02 22:42:40 +0800

    Add files via upload

---
+++ bresenham.m
@@ -0,0 +1,44 @@
+function l=bresenham(start_pnt,end_pnt)
+    
+    x0=start_pnt(1);
+    x1=end_pnt(1);
+    y0=start_pnt(2);
+    y1=end_pnt(2);
+    
+    dx=x1-x0;
+    dy=y1-y0;
+    
+    if abs(dx)>=abs(dy)
+        da=abs(dx);
+        db=abs(dy);
+        err=abs(db)/2;
+        dira=sign(dx);
+        dirb=sign(dy);        
+    else
+        da=abs(dy);
+        db=abs(dx);
+        err=abs(db)/2;
+        dira=sign(dx);
+        dirb=sign(dy);
+    end
+    
+    l=zeros(2,3000);
+    j=0;
+    cnt=1;
+    for i=0:da-1
+        err=err+db;
+        if err>=da
+            j=j+1;
+            err=err-da;
+        end 
+       
+        if abs(dx)>=abs(dy)   
+            l(:,cnt)=start_pnt+[dira*i;dirb*j];
+            cnt=cnt+1;
+        else
+            l(:,cnt)=start_pnt+[dira*j;dirb*i];
+            cnt=cnt+1;
+        end
+    end  
+    l(:,cnt:end)=[];    
+end
